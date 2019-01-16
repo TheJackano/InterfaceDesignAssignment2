4ca477c1cf97ac3e5c673b175318b784e3a918b8
TheJackano <jessenunes29@gmail.com>
2019-01-16 12:58:07 +0000

    new game screen has full functionaity (UI needs refining)

--- Interface Design - Assignment 1 (Unity Game)/Assets/Scripts/MenuController.cs
+++ Interface Design - Assignment 1 (Unity Game)/Assets/Scripts/MenuController.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public static string Username;
+    private bool isCharacterSelected;
+    public GameObject goCharacterOneSelected;
+    public InputField InputFieldUsername;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetUsername()
+    {
+        Username = InputFieldUsername.text;
+        Debug.Log(Username);
+    }
+    public void PlayGame()
+    {
+        if (goCharacterOneSelected.activeSelf == true && Username != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    public void ToggleMusic()
+    {
+        if (musicSource.isPlaying)
+            musicSource.Pause();
+        else
+            musicSource.Play();
+    }
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
 }
